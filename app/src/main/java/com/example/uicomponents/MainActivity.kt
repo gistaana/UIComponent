cf126a70f9e5cee88b703a4e29778c775d939f25
gistaana <tul00726@temple.edu>
2022-11-29 11:42:01 -0500

    initial commit

---
+++ app/src/main/java/com/example/uicomponents/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.uicomponents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
